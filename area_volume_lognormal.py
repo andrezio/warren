4ba8f455933b9aaf62f0b5656ecf7ed236608a4c
Andre <andrezio@live.com>
2017-02-19 17:10:35 -0300

    adicionado arquivos para lognormnal

---
+++ area_volume_lognormal.py
@@ -0,0 +1,35 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      Andrezio
+#
+# Created:     19/02/2017
+# Copyright:   (c) Andrezio 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#calculo log-normal
+from math import exp,log,sqrt
+Larea=1
+Lvolume=3
+
+Dzero=0
+
+Dzero = (8*Lvolume)/(9*Larea)
+
+Dzero = log(Dzero)
+
+Dzero = sqrt(Dzero)
+
+Dzero = exp(Dzero)
+
+Sigma = Dzero
+
+Dzero = pow( Dzero, 2)
+
+Dzero = -1*(5/2)*Dzero
+
+Dzero = exp(Dzero)
+
+Dzero = Dzero*(3/2)*Larea
