4145520b7f9e76ffaa7ba3d31d202280ead6162f
Luis Gallego Martinez <lgallego@ipen.br>
2017-02-21 10:35:02 -0300

    dados de L

--- area_volume_lognormal.py
+++ area_volume_lognormal.py
@@ -10,9 +10,13 @@
 #-------------------------------------------------------------------------------
 
 #calculo log-normal
-from math import exp,log,sqrt
-Larea=1
-Lvolume=3
+from math import exp,log,sqrt,pi
+from scipy import stats
+import numpy as np
+import matplotlib.pyplot as plt
+
+Larea=49.5082
+Lvolume=94.6759
 
 Dzero=0
 
@@ -33,3 +37,35 @@ Dzero = -1*(5/2)*Dzero
 Dzero = exp(Dzero)
 
 Dzero = Dzero*(3/2)*Larea
+
+def seq(start, stop, step=1):
+    n = int(round((stop - start)/float(step)))
+    if n > 1:
+        return([start + step*i for i in range(n+1)])
+    else:
+        return([])
+
+x=seq(0,Lvolume*2,0.1)
+
+pdf=[]
+for i in x:
+    valor = 0
+    try:
+        valor = log(i/Dzero)
+    except:
+        valor = 0
+    valor = valor/log(Sigma)
+    valor = valor*(-1)*(0.5)
+    valor=exp(valor)
+    try:
+       valor2 = 1/(sqrt(2*pi*pow(i,2)*pow(log(Sigma),2)))
+    except:
+       valor2 =0
+
+    valor= valor*valor2
+
+    pdf.append(   valor   )
+
+
+plt.plot(x,pdf,linestyle='-', marker='o')
+plt.show()
