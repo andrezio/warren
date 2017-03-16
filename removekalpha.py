fd78f1b0c1bf52a7da98cd7e3415a6bd77916633
Andre <andrezio@live.com>
2017-03-16 10:46:20 -0300

    atualizado zno

--- removekalpha.py
+++ removekalpha.py
@@ -18,5 +18,16 @@ namefile = 'ZnO_DEF_101_F_XY.xy'
 x,y = np.loadtxt(namefile, unpack= True)
 
 
+y1=[]
+for i in range(len(y)):
+    try:
+        y1.append(y[i]-y[i+1]/3)
+    except:
+        y1.append(y[i])
+
+
 plt.plot(x,y,linestyle='-', marker='o')
+
+plt.plot(x,y1,linestyle='-', marker='o')
+
 plt.show()
